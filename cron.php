ef1a1b80e5f8cbb1cef4ca33da59412d63c1a293
Tegos <mihavko_ivan@mail.ru>
2016-12-17 00:07:10 +0200

    Add Logger

--- cron.php
+++ cron.php
@@ -5,48 +5,56 @@ include(__DIR__ . '/class/autoloader.php');
 new AutoLoader();
 $model = new  Model();
 $helper = new  Helper();
+$log = new PHPLogger(__DIR__ . "/data/logs");
+$tag = "CRON";
 
+$log->i($tag, '---------------------');
+$log->i($tag, "Cron start");
+
+try {
 // cities
-$cities = $model->getCities();
+	$cities = $model->getCities();
 
 
 // sites
-$sites = $model->getSites();
+	$sites = $model->getSites();
 
-$site_arr = array();
-foreach ($sites as $site) {
-	$site_arr[] = $site['name'];
-}
-
-foreach ($sites as $site) {
-	$next_site = $helper->getNextSite($site_arr);
-
-	if ($site['name'] == $next_site) {
-		var_dump($next_site);
-		$site_id = $site['id'];
-		if (class_exists($site['name'])) {
-			$site_class = new $site['name']();
-
-			foreach ($cities as $city) {
-				$site_class->buildQuery($city);
-				$site_class->setSiteId($site['id']);
-				$site_class->setCityId($city['id']);
-				$site_class->addWeatherData();
-			}
-		}
-
-		$site_id++;
-		$next_site = $model->getSite($site_id);
-		while ($next_site && !$next_site['status']) {
-			$site_id++;
-			$next_site = $model->getSite($site_id);
-		}
-
-		$helper->setNextSite(@$next_site['name']);
-		break;
+	$site_arr = array();
+	foreach ($sites as $site) {
+		$site_arr[] = $site['name'];
 	}
 
+	foreach ($sites as $site) {
+		$next_site = $helper->getNextSite($site_arr);
+
+		if ($site['name'] == $next_site) {
+			$log->i($tag, "Site: {$next_site}");
+			$site_id = $site['id'];
+			if (class_exists($site['name'])) {
+				$site_class = new $site['name']();
+
+				foreach ($cities as $city) {
+					$site_class->buildQuery($city);
+					$site_class->setSiteId($site['id']);
+					$site_class->setCityId($city['id']);
+					$site_class->addWeatherData();
+				}
+			}
+
+			$site_id++;
+			$next_site = $model->getSite($site_id);
+			while ($next_site && !$next_site['status']) {
+				$site_id++;
+				$next_site = $model->getSite($site_id);
+			}
+
+			$helper->setNextSite(@$next_site['name']);
+			break;
+		}
+
+	}
+} catch (Exception $e) {
+	$log->e($tag, $e->getMessage());
 }
 
-echo "<br>Час виконання: " . round(microtime(true) - $start, 4) . ' с.';
-//mail('tegosiv@gmail.com', 'cron', 'Час виконання: ' . round(microtime(true) - $start, 4) . ' с.');
+$log->i($tag, 'Час виконання: ' . round(microtime(true) - $start, 4) . ' с.');
