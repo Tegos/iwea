d237a43c3cdd94f9e512a0e0b58c17b3c80ff4ec
Tegos <mihavko_ivan@mail.ru>
2016-12-18 13:37:06 +0200

    Come back old logic cron

--- class/interia.php
+++ class/interia.php
@@ -9,76 +9,81 @@
 class Interia extends Helper implements ISiteHelper
 {
 
-    var $url;
-    var $days = 8;
-    var $site_id;
-    var $city_id;
-    var $model;
+	private $url;
+	private $days = 8;
+	private $site_id;
+	private $city_id;
+	private $model;
 
-    public function __construct()
-    {
-        $this->model = new  Model();
-    }
+	public function __construct()
+	{
+		$this->model = new  Model();
+	}
 
-    public function buildQuery($city)
-    {
-        $city_name = $city['name_pl'];
-        $cid_pl = $city['cid_pl'];
+	public function buildQuery($city)
+	{
+		$city_name = $city['name_pl'];
+		$cid_pl = $city['cid_pl'];
 
-        $this->url = 'https://pogoda.interia.pl/';
-        $this->url .= "prognoza-dlugoterminowa-{$city_name},cId,{$cid_pl}";
-    }
-
-
-    public function addWeatherData()
-    {
-        $content = $this->get_web_page($this->url);
-        $html = simple_html_dom::str_get_html($content);
-
-        $lists = $html->find('.weather-forecast-longterm-list', 0);
-
-        $lies = $lists->children();
-
-        $i = 0;
-        foreach ($lies as $li) {
-            if ($li->tag == 'li') {
-                $data_insert = array();
-
-                $date = trim($li->find('.date', 0)->plaintext);
-                $date .= '.' . date('Y');
-
-                $DateTime = DateTime::createFromFormat('d.m.Y', $date);
-
-                $data_insert['site_id'] = (int)$this->site_id;
-                $data_insert['city_id'] = (int)$this->city_id;
-                $data_insert['date'] = $DateTime->format('Y-m-d');
-
-                $max = trim($li->find('.weather-forecast-longterm-list-entry-forecast-temp', 0)->plaintext);
-                $min = trim($li->find('.weather-forecast-longterm-list-entry-forecast-lowtemp', 0)->plaintext);
+		$this->url = 'https://pogoda.interia.pl/';
+		$this->url .= "prognoza-dlugoterminowa-{$city_name},cId,{$cid_pl}";
+	}
 
 
-                $data_insert['min_temp'] = (int)trim($min, '°C');
-                $data_insert['max_temp'] = (int)trim($max, '°C');;
+	public function addWeatherData()
+	{
+		$content = $this->get_web_page($this->url);
+		$html = simple_html_dom::str_get_html($content);
 
-                //$this->var_dump($data_insert);
-                $this->model->addWeatherRecord($data_insert);
+		if (!empty($html)) {
 
-                $i++;
+			$lists = $html->find('.weather-forecast-longterm-list', 0);
 
-            }
-        }
+			$lies = $lists->children();
 
-        $html->clear();
+			$i = 0;
+			foreach ($lies as $li) {
+				if ($li->tag == 'li') {
+					$data_insert = array();
 
-    }
+					$date = trim($li->find('.date', 0)->plaintext);
+					$date .= '.' . date('Y');
 
-    public function setSiteId($site_id)
-    {
-        $this->site_id = $site_id;
-    }
+					$DateTime = DateTime::createFromFormat('d.m.Y', $date);
 
-    public function setCityId($city_id)
-    {
-        $this->city_id = $city_id;
-    }
+					$data_insert['site_id'] = (int)$this->site_id;
+					$data_insert['city_id'] = (int)$this->city_id;
+					$data_insert['date'] = $DateTime->format('Y-m-d');
+
+					$max = trim($li->find('.weather-forecast-longterm-list-entry-forecast-temp', 0)->plaintext);
+					$min = trim($li->find('.weather-forecast-longterm-list-entry-forecast-lowtemp', 0)->plaintext);
+
+
+					$data_insert['min_temp'] = (int)trim($min, '°C');
+					$data_insert['max_temp'] = (int)trim($max, '°C');;
+
+					//$this->var_dump($data_insert);
+					$this->model->addWeatherRecord($data_insert);
+
+					$i++;
+
+				}
+			}
+
+			$html->clear();
+		} else {
+			trigger_error('Не доступний контетн');
+		}
+
+	}
+
+	public function setSiteId($site_id)
+	{
+		$this->site_id = $site_id;
+	}
+
+	public function setCityId($city_id)
+	{
+		$this->city_id = $city_id;
+	}
 }
--- cron.php
+++ cron.php
@@ -14,57 +14,54 @@ $log->i($tag, "Cron start");
 
 $state = $helper->getStateRun();
 
-if ($state != 'run') {
-	$helper->setStateRun('run');
-	try {
-		// cities
-		$cities = $model->getCities();
+//if ($state != 'run') {
+$helper->setStateRun('run');
+try {
+	// cities
+	$cities = $model->getCities();
 
-		// sites
-		$sites = $model->getSites();
+	// sites
+	$sites = $model->getSites();
 
-		$site_arr = array();
-		foreach ($sites as $site) {
-			$site_arr[] = $site['name'];
-		}
-
-		foreach ($sites as $site) {
-			$next_site = $helper->getNextSite($site_arr);
-
-			if ($site['name'] == $next_site) {
-
-				$site_id = $site['id'];
-				if (class_exists($site['name'])) {
-					$log->i($tag, "Site: {$next_site}");
-					$site_class = new $site['name']();
-
-					foreach ($cities as $city) {
-						$site_class->buildQuery($city);
-						$site_class->setSiteId($site['id']);
-						$site_class->setCityId($city['id']);
-						$site_class->addWeatherData();
-					}
-				}
-
-				$site_id++;
-				$next_site = $model->getSite($site_id);
-				while ($next_site && !$next_site['status']) {
-					$site_id++;
-					$next_site = $model->getSite($site_id);
-				}
-
-				$helper->setNextSite(@$next_site['name']);
-				$helper->setStateRun('');
-				break;
-			}
-
-		}
-	} catch (Exception $e) {
-		$helper->setStateRun('');
-		$log->e($tag, $e->getMessage());
+	$site_arr = array();
+	foreach ($sites as $site) {
+		$site_arr[] = $site['name'];
 	}
-} else {
-	$log->w($tag, 'Запущений інший крон');
+
+	foreach ($sites as $site) {
+		//$next_site = $helper->getNextSite($site_arr);
+		//if ($site['name'] == $next_site) {
+
+		$site_id = $site['id'];
+		if (class_exists($site['name'])) {
+			$log->i($tag, "Site: {$site['name']}");
+			$site_class = new $site['name']();
+
+			foreach ($cities as $city) {
+				$site_class->buildQuery($city);
+				$site_class->setSiteId($site['id']);
+				$site_class->setCityId($city['id']);
+				$site_class->addWeatherData();
+			}
+		}
+
+		$site_id++;
+		$next_site = $model->getSite($site_id);
+		while ($next_site && !$next_site['status']) {
+			$site_id++;
+			$next_site = $model->getSite($site_id);
+		}
+
+		$helper->setNextSite(@$next_site['name']);
+		$helper->setStateRun('');
+		//break;
+		//}
+
+	}
+} catch (Exception $e) {
+	$helper->setStateRun('');
+	$log->e($tag, $e->getMessage());
 }
+
 
 $log->i($tag, 'Час виконання: ' . round(microtime(true) - $start, 4) . ' с.');
